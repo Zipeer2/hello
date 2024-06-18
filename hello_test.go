5556c72dd857bf40ab6928b2a6236b9e3bb87c7f
Vladislav <proskyr.don@gmail.com>
2024-06-18 16:47:32 +0300

    Initial commit

---
+++ hello_test.go
@@ -0,0 +1,19 @@
+package hello_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/Zipeer2/hello"
+)
+
+func TestPrintToPrintsHelloMessageToWriter(t *testing.T) {
+	t.Parallel()
+	var buf bytes.Buffer
+	hello.PrintTo(&buf)
+	got := buf.String()
+	want := "Hello, world\n"
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
