1779ce3e0abc902381d9345fe25bde4cce6e8f95
Vladislav <proskyr.don@gmail.com>
2024-06-19 12:04:10 +0300

    Add files

--- cmd/main.go
+++ cmd/main.go
@@ -1,1 +1,11 @@
 package main
+
+import (
+	"os"
+
+	"github.com/Zipeer2/hello"
+)
+
+func main() {
+	hello.PrintTo(os.Stdout)
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module hello
+
+go 1.22.4
+
+require github.com/Zipeer2/hello v0.0.0-20240618134732-5556c72dd857
